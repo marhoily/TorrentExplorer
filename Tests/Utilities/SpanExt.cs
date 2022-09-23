79d4dd86921c306b99d9ec5007b4b2fa984e4b0e
Ilya <marhoily@gmail.com>
2022-09-23 15:07:13 +0300

    + Spank Ext

--- Tests/Utilities/SpanExt.cs
+++ Tests/Utilities/SpanExt.cs
@@ -1,6 +1,4 @@
-﻿using FluentAssertions;
-
-namespace Tests.Utilities;
+﻿namespace Tests.Utilities;
 
 public static class SpanExtensions
 {
@@ -8,9 +6,13 @@ public static class SpanExtensions
     {
         return new LineSplitEnumerator(str);
     }
+    public static SplitEnumerator1Exact SplitExact(this ReadOnlySpan<char> str, char delimiter)
+    {
+        return new SplitEnumerator1Exact(str, delimiter);
+    }
     public static SplitEnumerator1 Split(this ReadOnlySpan<char> str, char delimiter)
     {
-        return new SplitEnumerator1(str,delimiter);
+        return new SplitEnumerator1(str, delimiter);
     }
 
     // Must be a ref struct as it contains a ReadOnlySpan<char>
@@ -80,7 +82,7 @@ public static class SpanExtensions
 
         public bool MoveNext()
         {
-            var index = 0;
+            int index;
             do
             {
                 if (_str.Length == 0) // Reach the end of the string
@@ -101,43 +103,57 @@ public static class SpanExtensions
 
                 _str = _str.Slice(index);
             } while (index < _str.Length - 1 && Current.IsEmpty);
-         
+
             return !Current.IsEmpty;
         }
 
         public ReadOnlySpan<char> Current { get; private set; }
     }
 
-}
-
-public sealed class SplitLinesTests
-{
-    [Fact]
-    public void SplitLines()
+    // Must be a ref struct as it contains a ReadOnlySpan<char>
+    public ref struct SplitEnumerator1Exact
     {
-        Split("a\nb").Should().Equal("a", "b");
+        private ReadOnlySpan<char> _str;
+        private readonly char _delimiter;
+        private bool _extraEmpty;
+
+        public SplitEnumerator1Exact(ReadOnlySpan<char> str, char delimiter)
+        {
+            _str = str;
+            _delimiter = delimiter;
+            Current = default;
+            _extraEmpty = false;
+        }
+
+        // Needed to be compatible with the foreach operator
+        public SplitEnumerator1Exact GetEnumerator() => this;
+
+        public bool MoveNext()
+        {
+            if (_str.IsEmpty && _extraEmpty)
+            {
+                Current = _str;
+                _extraEmpty = false;
+                return true;
+            }
+            if (_str.IsEmpty) // Reach the end of the string
+                return false;
+
+            var index = _str.IndexOf(_delimiter);
+            if (index == -1) // The string is composed of only one line
+            {
+                _extraEmpty = false;
+                Current = _str;
+                _str = ReadOnlySpan<char>.Empty; // The remaining string is an empty string
+                return true;
+            }
+            _extraEmpty = true;
+            Current = _str[..index];
+            _str = _str[(index+1)..];
+            return true;
+        }
+
+        public ReadOnlySpan<char> Current { get; private set; }
     }
 
-    [Fact] public void Split1_04() => Split("a;b",';').Should().Equal("a", "b");
-    [Fact] public void Split1_05() => Split(";",';').Should().Equal();
-    [Fact] public void Split1_06() => Split(";;",';').Should().Equal();
-    [Fact] public void Split1_07() => Split("a;;b",';').Should().Equal("a", "b");
-    [Fact] public void Split1_08() => Split("a;;;b",';').Should().Equal("a","b");
-    [Fact] public void Split1_09() => Split("aaa;b;",';').Should().Equal("aaa","b");
-    [Fact] public void Split1_1O() => Split(";a;b",';').Should().Equal("a","b");
-
-    public static List<string> Split(string input)
-    {
-        List<string> result = new();
-        foreach (var line in input.AsSpan().SplitLines()) 
-            result.Add(line.ToString());
-        return result;
-    }
-    public static List<string> Split(string input, char delimiter)
-    {
-        List<string> result = new();
-        foreach (var line in input.AsSpan().Split(delimiter)) 
-            result.Add(line.ToString());
-        return result;
-    }
 }
---
+++ Tests/Utilities/SplitLinesTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+
+namespace Tests.Utilities;
+
+public sealed class SplitLinesTests
+{
+    [Fact]
+    public void SplitLines()
+    {
+        Split("a\nb").Should().Equal("a", "b");
+    }
+
+    [Fact] public void Split1_03() => Split("a", ';').Should().Equal("a");
+    [Fact] public void Split1_04() => Split("a;b", ';').Should().Equal("a", "b");
+    [Fact] public void Split1_05() => Split(";", ';').Should().Equal();
+    [Fact] public void Split1_06() => Split(";;", ';').Should().Equal();
+    [Fact] public void Split1_07() => Split("a;;b", ';').Should().Equal("a", "b");
+    [Fact] public void Split1_08() => Split("a;;;b", ';').Should().Equal("a", "b");
+    [Fact] public void Split1_09() => Split("aaa;b;", ';').Should().Equal("aaa", "b");
+    [Fact] public void Split1_1O() => Split(";a;b", ';').Should().Equal("a", "b");
+
+    [Fact] public void SplitExact1_02() => SplitExact("a", ';').Should().Equal("a");
+    [Fact] public void SplitExact1_03() => SplitExact("a;", ';').Should().Equal("a", "");
+    [Fact] public void SplitExact1_04() => SplitExact("a;;", ';').Should().Equal("a", "", "");
+    [Fact] public void SplitExact1_05() => SplitExact(";", ';').Should().Equal("", "");
+    [Fact] public void SplitExact1_06() => SplitExact(";;", ';').Should().Equal("", "", "");
+    [Fact] public void SplitExact1_07() => SplitExact("a;;b", ';').Should().Equal("a", "", "b");
+    [Fact] public void SplitExact1_08() => SplitExact("a;;;b", ';').Should().Equal("a", "", "", "b");
+    [Fact] public void SplitExact1_09() => SplitExact("aaa;b;", ';').Should().Equal("aaa", "b", "");
+    [Fact] public void SplitExact1_1O() => SplitExact(";a;b", ';').Should().Equal("", "a", "b");
+
+    public static List<string> Split(string input)
+    {
+        List<string> result = new();
+        foreach (var line in input.AsSpan().SplitLines())
+            result.Add(line.ToString());
+        return result;
+    }
+    public static List<string> Split(string input, char delimiter)
+    {
+        List<string> result = new();
+        foreach (var line in input.AsSpan().Split(delimiter))
+            result.Add(line.ToString());
+        return result;
+    }
+    public static List<string> SplitExact(string input, char delimiter)
+    {
+        List<string> result = new();
+        foreach (var line in input.AsSpan().SplitExact(delimiter))
+            result.Add(line.ToString());
+        return result;
+    }
+}
