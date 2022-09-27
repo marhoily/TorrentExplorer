75e26c8125337f6f614a9524b846cf6ed5266bb6
Ilya <marhoily@hotmail.com>
2022-09-27 03:49:46 +0300

    AuthorFixerExt += Simplify

--- Tests/Flibusta/FixAuthorTests.cs
+++ Tests/Flibusta/FixAuthorTests.cs
@@ -9,7 +9,7 @@ public sealed class FixAuthorTests
     public const string OutputUnrecognized = @"C:\temp\TorrentsExplorerData\Extract\Rutracker\authors-unrecognized.json";
 
     [Fact]
-    public async Task LocalAuthorsToAtoms()
+    public async Task FixAuthors()
     {
         var fixData = await @"c:\temp\TorrentsExplorerData\Extract\AuthorData.json"
             .ReadJson<AuthorData[]>();
@@ -34,6 +34,9 @@ public static class AuthorFixerExt
                 _ => throw new ArgumentOutOfRangeException()
             };
     }
+
+    public static string Simplify(this string input) => 
+        input.Replace('Ё', 'Е').Replace('ё', 'е');
 }
 public sealed class AuthorFixer
 {
@@ -45,32 +48,32 @@ public sealed class AuthorFixer
     {
         _byFirstName = authors
             .Where(a => a.FirstName != null)
-            .ToLookup(a => a.FirstName!);
+            .ToLookup(a => a.FirstName!.Simplify());
         _byMiddleName = authors
             .Where(a => a.MiddleName != null)
-            .ToLookup(a => a.MiddleName!);
+            .ToLookup(a => a.MiddleName!.Simplify());
         _byLastName = authors
             .Where(a => a.LastName != null)
-            .ToLookup(a => a.LastName!);
+            .ToLookup(a => a.LastName!.Simplify());
     }
 
     public PurifiedAuthor Fix(FirstLast input)
     {
-        if (_byFirstName.Contains(input.FirstName) &&
-            _byLastName.Contains(input.LastName))
+        if (_byFirstName.Contains(input.FirstName.Simplify()) &&
+            _byLastName.Contains(input.LastName.Simplify()))
             return input;
 
-        if (_byFirstName.Contains(input.LastName) &&
-            _byLastName.Contains(input.FirstName))
+        if (_byFirstName.Contains(input.LastName.Simplify()) &&
+            _byLastName.Contains(input.FirstName.Simplify()))
             return new FirstLast(
                 input.TopicId, input.LastName, input.FirstName);
 
         if (input.FirstName.Contains(' '))
         {
-            var parts = input.FirstName.Split(' ');
+            var parts = input.FirstName.Simplify().Split(' ');
             if (_byFirstName.Contains(parts[0]) &&
                 _byMiddleName.Contains(parts[1]) &&
-                _byLastName.Contains(input.LastName))
+                _byLastName.Contains(input.LastName.Simplify()))
                 return new ThreePartsName(
                     input.TopicId, parts[0], parts[1], input.LastName);
 
