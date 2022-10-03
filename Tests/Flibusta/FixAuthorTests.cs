09c7770257f7d4f45112165a3f2eb3a91a0e3f3e
Ilya <marhoily@gmail.com>
2022-10-03 06:01:40 +0300

    WithHeader

--- Tests/Flibusta/FixAuthorTests.cs
+++ Tests/Flibusta/FixAuthorTests.cs
@@ -6,7 +6,9 @@ namespace Tests.Flibusta;
 public sealed class FixAuthorTests
 {
     public const string Output = @"C:\temp\TorrentsExplorerData\Extract\Rutracker\authors-fixed.json";
-    public const string OutputUnrecognized = @"C:\temp\TorrentsExplorerData\Extract\Rutracker\authors-unrecognized.json";
+
+    public const string OutputUnrecognized =
+        @"C:\temp\TorrentsExplorerData\Extract\Rutracker\authors-unrecognized.json";
 
     [Fact]
     public async Task FixAuthors()
@@ -16,28 +18,54 @@ public sealed class FixAuthorTests
         var fixer = new AuthorFixer(fixData!);
 
         var rutracker = await AuthorExtractionTests
-            .Output.ReadTypedJson<PurifiedAuthor[]>();
+            .Output.ReadTypedJson<WithHeader<PurifiedAuthor>[]>();
         var result = fixer.Fix(rutracker!).ToList();
         await Output.SaveTypedJson(result);
-        await OutputUnrecognized.SaveTypedJson(result.OfType<UnrecognizedFirstLast>());
+        await OutputUnrecognized.SaveTypedJson(result
+            .OfType<PurifiedAuthor, UnrecognizedFirstLast>()
+            .GroupBy(fl => new { fl.Payload.FirstName, fl.Payload.LastName })
+            .Select(g => new
+            {
+                g.Key.FirstName, g.Key.LastName,
+                Topics = g.Select(fl => fl.TopicId).ToArray()
+            }));
     }
 }
+
 public static class AuthorFixerExt
 {
-    public static IEnumerable<PurifiedAuthor> Fix(this AuthorFixer authorFixer, PurifiedAuthor[] src)
+    public static IEnumerable<WithHeader<TOutput>> OfType<TInput, TOutput>(
+        this IEnumerable<WithHeader<TInput>> src) where TOutput:TInput
+    {
+        foreach (var item in src)
+            if (item.Payload is TOutput output)
+                yield return new WithHeader<TOutput>(item.TopicId, output);
+    }
+    public static IEnumerable<WithHeader<PurifiedAuthor>> Fix(
+        this AuthorFixer authorFixer, WithHeader<PurifiedAuthor>[] src)
     {
         foreach (var author in src)
-            yield return author switch
+            yield return author with
+            {
+                Payload = One(author.Payload)
+            };
+
+        PurifiedAuthor One(PurifiedAuthor purifiedAuthor)
+        {
+            return purifiedAuthor switch
             {
                 FirstLast fl => authorFixer.Fix(fl),
                 Only o => authorFixer.Fix(o),
-                _ => throw new ArgumentOutOfRangeException()
+                WithMoniker m => new WithMoniker(One(m.RealName), One(m.Moniker)),
+                _ => throw new ArgumentOutOfRangeException(purifiedAuthor.ToString())
             };
+        }
     }
 
-    public static string Simplify(this string input) => 
+    public static string Simplify(this string input) =>
         input.Replace('Ё', 'Е').Replace('ё', 'е');
 }
+
 public sealed class AuthorFixer
 {
     private readonly ILookup<string, AuthorData> _byFirstName;
@@ -75,9 +103,19 @@ public sealed class AuthorFixer
             if (_byFirstName.Contains(parts[0]) &&
                 _byMiddleName.Contains(parts[1]) &&
                 _byLastName.Contains(lastName))
-                return new ThreePartsName(parts[0], parts[1], input.LastName);
-
+                return new ThreePartsName(
+                    parts[0], parts[1], input.LastName);
         }
+
+        /* {
+    "$type": "Tests.Rutracker.FirstLast, Tests",
+    "FirstName": "Г.А.",
+    "LastName": "Зотов",
+    "TopicId": 2713291
+  }*/
+        if (input.LastName == "Зотов")
+            1.ToString();
+
         return new UnrecognizedFirstLast(input.FirstName, input.LastName);
     }
 
