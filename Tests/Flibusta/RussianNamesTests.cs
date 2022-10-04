fdebc91d7cc9507fd156eeb1bd2b85cdbc087abd
Ilya <marhoily@gmail.com>
2022-10-04 04:28:42 +0300

    Better russian name recognition

--- Tests/Flibusta/RussianNamesTests.cs
+++ Tests/Flibusta/RussianNamesTests.cs
@@ -2,6 +2,8 @@
 using CsvHelper;
 using CsvHelper.Configuration;
 using FluentAssertions;
+using Newtonsoft.Json;
+using ServiceStack;
 using Tests.Utilities;
 
 namespace Tests.Flibusta;
@@ -21,6 +23,12 @@ public sealed class RussianNamesTests
         string Surname, string Sex,
         int PeoplesCount,
         string WhenPeoplesCount, string Source);
+    public sealed record JFirstName(string Text, int Count, string[] Ethnic, char Gender);
+    public sealed record JLastName(
+        string Text, string FName,
+        [JsonProperty("f_form")]
+        string FForm,
+        int Count, char Gender);
 
     [Fact]
     public async Task Save()
@@ -37,8 +45,8 @@ public sealed class RussianNamesTests
             .OrderByDescending(x => x.PeoplesCount)
             .Select(x => x.Surname)
             .Take(5).StrJoin().Should().Be("Школа, Москва, Московская, Петербург, Ленинградская");
-        var ff = firstNames.Select(x => x.Name).ToHashSet();
-        var ll = lastNames.Select(x => x.Surname).ToHashSet();
+        var ff = Enumerable.ToHashSet(firstNames.Select(x => x.Name));
+        var ll = Enumerable.ToHashSet(lastNames.Select(x => x.Surname));
         ff.Count(f => ll.Contains(f)).Should().Be(3765);
         ff.ExceptWith(lastNames.Select(x => x.Surname));
         ll.ExceptWith(firstNames.Select(x => x.Name));
@@ -56,4 +64,74 @@ public sealed class RussianNamesTests
             });
         return csv.GetRecords<T>().ToList();
     }
+
+    [Fact]
+    public async Task ReadJFirstNames()
+    {
+        var firstNames = JRead<JFirstName>(@"C:\temp\TorrentsExplorerData\names.jsonl");
+        firstNames.Count.Should().Be(32134);
+        firstNames.Select(n => n.Text).Distinct().Count().Should().Be(32134);
+        firstNames.OrderByDescending(n => n.Count).Take(5)
+            .Select(n => n.Text).StrJoin().Should()
+            .Be("Александр, Николай, Иван, Сергей, Владимир");
+
+        var lastNames = JRead<JLastName>(@"C:\temp\TorrentsExplorerData\surnames.jsonl");
+        lastNames.Count.Should().Be(375449);
+        lastNames.Select(n => n.Text).Distinct().Count().Should().Be(375449);
+        lastNames.OrderByDescending(n => n.Count).Take(5)
+            .Select(n => n.Text).StrJoin().Should()
+            .Be("Иванов, Иванова, Кузнецов, Смирнов, Попов");
+
+        var ff = firstNames
+                .GroupBy(x => x.Text.ToLowerInvariant().Simplify())
+                .Select(g => new
+                {
+                    g.Key,
+                    Value = g.MaxBy(n => n.Count)!.Text.ToLowerInvariant().ToPascalCase(),
+                    Count = g.Sum(x => x.Count),
+                    T = "F"
+                });
+        var ll = lastNames
+                .GroupBy(x => x.Text.ToLowerInvariant().Simplify())
+                .Select(g => new
+                {
+                    g.Key,
+                    Value = g.MaxBy(n => n.Count)!.Text.ToLowerInvariant().ToPascalCase(),
+                    Count = g.Sum(x => x.Count),
+                    T = "L"
+                });
+        var lookup = ff.Concat(ll)
+            .GroupBy(x => x.Key)
+            .Select(g =>
+            {
+                var l = g.ToList();
+                if (l.Count == 1) return l[0];
+                if (l.Count == 2)
+                {
+                    if (l[0].Count * 3 < l[1].Count) return l[1];
+                    if (l[1].Count * 3 < l[0].Count) return l[0];
+                    return null;
+                }
+
+                throw new Exception(l.StrJoin());
+            })
+            .WhereNotNull()
+            .ToLookup(x => x.T, x => x.Value);
+
+
+        await Known.SaveJson(new KnownNames(
+            new HashSet<string>(lookup["F"]),
+            new HashSet<string>(lookup["L"])));
+
+    }
+
+    private static List<T> JRead<T>(string path)
+    {
+        return path
+            .ReadAllText()
+            .ReadLines()
+            .Select(l => l.FromJson<T>())
+            .ToList();
+    }
+
 }
